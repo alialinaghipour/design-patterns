affb0ce062cbc7c7299bbd47166372c5f0543608
ali alinaghipour <alinaqipourali@gmail.com>
2021-12-26 10:10:39 -0800

    version 1

---
+++ Structural/Decorator/Decorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using Decorator;
+
+var largeSandwich = new LargeSandwich();
+Console.WriteLine(largeSandwich.GetPrice());
+
+var mediumSandwich=new MediumSandwich();
+Console.WriteLine(mediumSandwich.GetPrice());
+
+var smallSandwich = new SmallSandwich();
+Console.WriteLine(smallSandwich.GetPrice());
+
+
---
+++ Structural/Decorator/Decorator/Sandwich.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class Sandwich
+    {
+        public abstract string GetPrice();
+    }
+
+    public class LargeSandwich : Sandwich
+    {
+        public override string GetPrice()
+        {
+            return "LargeSandwich ==> 300";
+        }
+    }
+
+    public class MediumSandwich : Sandwich
+    {
+        public override string GetPrice()
+        {
+            return "MediumSandwich ==> 200";
+        }
+    }
+
+    public class SmallSandwich : Sandwich
+    {
+        public override string GetPrice()
+        {
+            return "SmallSandwich ==> 100";
+        }
+    }
+}
